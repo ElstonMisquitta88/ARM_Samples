bb6ff5cd12ae0a72a69ffd85431666a66caa3efc
Elston Misquitta <elston007@gmail.com>
2022-12-20 21:58:50 +0530

    New Samples added

--- Common_Commands.ps1
+++ Common_Commands.ps1
@@ -1,4 +1,7 @@
 az login
 
+# create resource group
+az group create -l westindia -n app-grp
+
 # Deployment of ARM Template
 az deployment group create --resource-group app-grp --template-file 01_StorageAccount.json
