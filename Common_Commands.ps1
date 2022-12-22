961834e134757127d01bcc6bf308f537c4d0a4c0
Elston Misquitta <elston007@gmail.com>
2022-12-22 23:11:46 +0530

    Changes

--- Common_Commands.ps1
+++ Common_Commands.ps1
@@ -3,5 +3,10 @@ az login
 # create resource group
 az group create -l westindia -n app-grp
 
+
 # Deployment of ARM Template
 az deployment group create --resource-group app-grp --template-file 01_StorageAccount.json
+
+
+# Deployment of ARM Template with parameter File
+az deployment group create --resource-group app-grp --template-file 07_Template_Withparam.json --parameters Parameter_For_07_Template.json
